f3d5a22d0f32f1ad5916ada2db182603f38e3e5e
Eric Driussi <ericdriussi@gmail.com>
2023-09-23 10:49:50 +0100

    rust

---
+++ rust/src/operator/mod.rs
@@ -0,0 +1,6 @@
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod test;
---
+++ rust/src/operator/test.rs
@@ -0,0 +1,8 @@
+use super::*;
+
+#[test]
+fn should_sum() {
+    let result = sum(2, 3);
+    assert_eq!(result, 5);
+}
+
